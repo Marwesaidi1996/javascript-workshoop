3a83472c620cdb12a6d7f4a929d4e08bd5d07f0c
Marwen <marwensaidi812@gmail.com>
2024-09-10 17:12:22 +0200

    workshoop

--- script.js
+++ script.js
@@ -78,12 +78,11 @@ function sumArray(arr) {
     let sum = 0
     for (let i = 0; i < arr.length; i++) {
         sum = sum + arr[i]
-
-    }
+}
     return sum
 }
-const arr = [1, 2, 3, 4, 5];
-const sum = sumArray(numbers);
+const arr = [1,2,3,4,5,6,7,8,9];
+const sum= sumArray(arr);
 console.log("sumArray", sum);
 
 //function n6.......
